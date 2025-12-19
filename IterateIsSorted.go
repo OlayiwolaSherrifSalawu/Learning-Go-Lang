89a81ace1ad7b4a3fd4248841cf4672a58dd9d35
Salawu Olayiwola Sherrif <salawuolayiwola181@gmail.com>
2025-12-19 23:08:05 +0100

    Checking is sorted with recursive and iterative method

--- IterateIsSorted.go
+++ IterateIsSorted.go
@@ -3,7 +3,7 @@ package layi
 func IterateIsSorted(arr []int) bool {
 	for i := 0; i < len(arr); i++ { //O(n)
 		for j := i; j < i+1; j++ { //O(logn)
-			if i+1 < len(arr) {
+			if j+1 < len(arr) {
 				if arr[j+1] < arr[i] {
 					return false
 				}
