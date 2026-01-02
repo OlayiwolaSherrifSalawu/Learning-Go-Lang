4660f92287dd852bfe2a72aa40b2df77c325e6ed
Salawu Olayiwola Sherrif <salawuolayiwola181@gmail.com>
2026-01-02 11:55:13 +0100

    Add: delet functions for a doubly linked list

--- DataStructures/LinkedList/CircularList.go
+++ DataStructures/LinkedList/CircularList.go
@@ -18,22 +18,45 @@ func CountNode(l *Clist) {
 		return
 	}
 	current := l.head
-	for current.next != l.head {
+	for current != l.head {
 		count++
 		current = current.next
 	}
 	fmt.Println(count)
 }
 func insertCf(l *Clist, data int) {
+	node := &Cll{data: data}
+	if l.head == nil {
+		l.head = node
+	}
+	node.next = l.head
+	thehead := l.head
+	for thehead.next != l.head {
+		node.next = node
+		thehead = thehead.next
+	}
+	node.next = l.head
+	thehead.next = node
 
 }
 
 func main() {
 	node := &Clist{}
-	if node.head == nil {
-		return
-	}
+	insertCf(node, 1)
+	insertCf(node, 2)
+	insertCf(node, 3)
+	insertCf(node, 4)
+	insertCf(node, 5)
+	insertCf(node, 6)
+	insertCf(node, 7)
+	insertCf(node, 8)
 
 	CountNode(node)
-
+	current := node.head
+	for current.next != node.head {
+		fmt.Print(current.data)
+		fmt.Print("->")
+		current = current.next
+	}
+	fmt.Println()
 }
