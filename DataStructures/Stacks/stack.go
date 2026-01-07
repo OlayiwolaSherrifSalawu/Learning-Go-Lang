2fb88355d8b990ee2fe3e331610648239fb3ed30
Salawu Olayiwola Sherrif <salawuolayiwola181@gmail.com>
2026-01-07 23:22:09 +0100

    solving a stack problem

--- DataStructures/Stacks/stack.go
+++ DataStructures/Stacks/stack.go
@@ -52,3 +52,26 @@ func DeleteStack(S *ArrayStack) {
 // algorithm is very useful in compilers. Each time the parser reads one character at a time. If the
 // character is an opening delimiter such as (, {, or [- then it is written to the stack. When a closing
 // delimiter is encountered like ), }, or ]-the stack is popped.
+
+/*The opening and closing delimiters are then compared. If they match, the parsing of the string
+continues. If they do not match, the parser indicates that there is an error on the line. A linear-time
+O(n) algorithm based on stack can be given as:
+Algorithm:
+a) Create a stack.
+b) while (end of input is not reached) {
+1) If the character read is not a symbol to be balanced, ignore it.
+2) If the character is an opening symbol like (, [, {, push it onto the stack
+3) If it is a closing symbol like ),],}, then if the stack is empty report an
+error. Otherwise pop the stack.
+4) If the symbol popped is not the corresponding opening symbol, report an
+error.
+}
+c) At end of input, if the stack is not empty report an error*/
+
+func balance(Input string) {
+	lenGt := len(Input)
+	stack := createStack()
+	for i := 0; i < lenGt; i++ {
+
+	}
+}
